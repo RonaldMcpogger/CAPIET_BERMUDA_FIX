705eafc0fe07457c80876d7ab08e9b84c4b20e69
ParfaitOpGuide <nathan_agasen@dlsu.edu.ph>
2026-01-21 17:26:17 +0800

    bugfix

--- Assets/Scripts/RotateObject.cs
+++ Assets/Scripts/RotateObject.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class RotateObject : MonoBehaviour
 {
+    float target1, target2;
     // Start is called before the first frame update
     void Start()
     {
-        
+        target1 = -.3f;
+        target2 = .3f;
     }
 
     // Update is called once per frame
     void Update()
     {
         this.gameObject.transform.Rotate(0,1,0);
+        
     }
 }
