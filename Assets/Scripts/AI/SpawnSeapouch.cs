cfb3d320c69bf2df64f1090e50e74548576c26c7
Za13th <156149739+Za13th@users.noreply.github.com>
2026-02-11 17:38:17 +0800

    Fixed Special Seapouch Spawn and Reenabled Capsule UI

--- Assets/Scripts/AI/SpawnSeapouch.cs
+++ Assets/Scripts/AI/SpawnSeapouch.cs
@@ -8,9 +8,9 @@ public class SpawnSeapouch : MonoBehaviour
     [SerializeField] private GameObject spawnTrigger;
     [SerializeField] private GameObject despawnTrigger;
     [SerializeField] private GameObject player;
+    [SerializeField] private GameObject coordinates;
 
-    [SerializeField] private float maxTimer = 0;
-    [SerializeField] private float currentTimer = 0;
+    [SerializeField] private bool special = false;
 
     bool spawned = false;
 
@@ -18,7 +18,6 @@ public class SpawnSeapouch : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        currentTimer = 0;
         seapouchEnemy.SetActive(false);
         spawned = false;
     }
@@ -26,9 +25,10 @@ public class SpawnSeapouch : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        currentTimer += Time.deltaTime;
+        if (special && coordinates.activeSelf == false)
+            special = false;
 
-        if (currentTimer >= maxTimer && !spawned && spawnTrigger.GetComponent<Collider>().bounds.Intersects(player.GetComponent<Collider>().bounds))
+        if (!special && !spawned && spawnTrigger.GetComponent<Collider>().bounds.Intersects(player.GetComponent<Collider>().bounds))
         {
             seapouchEnemy.SetActive(true);
             spawned = true;
