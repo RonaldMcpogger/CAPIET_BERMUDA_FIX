e3f98002c9b8328226c370b03f81d25431574516
Za13th <156149739+Za13th@users.noreply.github.com>
2026-04-09 02:55:48 +0800

    Added More Vibrations and Screen Shakes during the Final Level and When the Seapouch is Spawned
    Check the Scripts for Specific Changes.

--- Assets/Scripts/AI/FireworksFlyover.cs
+++ Assets/Scripts/AI/FireworksFlyover.cs
@@ -1,10 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class FireworksFlyover : MonoBehaviour
 {
     [SerializeField] private float speed = 5;
+
+    [SerializeField] bool decrease = true;
+    [SerializeField] bool up = true;
+    [SerializeField] bool once = false;
+    [SerializeField] float timerMax = 10;
+    [SerializeField] float timer = 0;
+    [SerializeField] float intensity = 2.0f;
+    [SerializeField] float motorIntensity = 0.4f;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,5 +26,42 @@ public class FireworksFlyover : MonoBehaviour
     void Update()
     {
         this.transform.position += this.transform.forward * Time.deltaTime * speed;
+        
+        if (!once) //At the Start, enable vibrations and screenshake.
+        {
+            if (Gamepad.current != null)
+                Gamepad.current.SetMotorSpeeds(0, 0.4f);
+
+            GlobalScreenShake.Instance.TriggerShake(timerMax, intensity);
+            once = true;
+        }
+
+        if (up && timer <= timerMax) //Continues screenshake and vibrations until timer is up
+        {
+            timer += Time.deltaTime;
+            if (timer > timerMax)
+            {
+                Gamepad.current.SetMotorSpeeds(0, 0.0f);
+                up = false;
+            }
+        }
+        else if (!up)//when timer is up, the timer goes again but this time as a cooldown, and restarts the screenshakes when everything is over.
+        {
+            timer -= Time.deltaTime;
+            if (timer <= 0)
+            {
+                timer = 0.0f;
+                up = true;
+                once = false;
+
+                if (decrease)// halves the intensity of the screenshake and vibrations each loop for the first fireworks monster
+                {
+                    intensity /= 2;
+                    motorIntensity /= 2;
+                }
+
+            }
+        }
+
     }
 }
--- Assets/Scripts/AI/SpawnSeapouch.cs
+++ Assets/Scripts/AI/SpawnSeapouch.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class SpawnSeapouch : MonoBehaviour
 {
@@ -13,6 +14,9 @@ public class SpawnSeapouch : MonoBehaviour
     [SerializeField] private bool special = false;
 
     bool spawned = false;
+    bool once = false;
+    [SerializeField] float timerMax = 3;
+    [SerializeField] float timer = 0;
 
 
     // Start is called before the first frame update
@@ -26,8 +30,23 @@ public class SpawnSeapouch : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (special && coordinates.activeSelf == false)
+        if (special && coordinates.activeSelf == false && !once) //Shakes the Screen and Starts Controller Vibrations when the Coordinates are picked up
+        {
+            once = true;
+
+            if (Gamepad.current != null)
+                Gamepad.current.SetMotorSpeeds(0, 0.4f);
+
+            GlobalScreenShake.Instance.TriggerShake(3.0f, 3.0f); //3 Second Duration, so it syncs with the vibrations
             special = false;
+        }
+
+        if(once && timer <= timerMax) //Stops Controller Vibration after the 3 second timer is up
+        {
+            timer += Time.deltaTime;
+            if (timer > timerMax) Gamepad.current.SetMotorSpeeds(0, 0.4f); 
+        }
+            
 
         if (!special && !spawned && spawnTrigger.GetComponent<Collider>().bounds.Intersects(player.GetComponent<Collider>().bounds))
         {
